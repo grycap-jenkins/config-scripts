1540a5f5ec5b8a6d23dff3a9708e591d8600df26
Jenkins <grycap-jenkins@users.noreply.github.com>
2016-04-26 15:16:49 +0200

    Update configure-tests-ports.sh

--- im/configure-tests-ports.sh
+++ im/configure-tests-ports.sh
@@ -11,3 +11,5 @@ sed -i "s/TEST_PORT = 8899/TEST_PORT = $XMLRPC_PORT/g" QuickTestIM.py
 REST_PORT=$(docker port im-grycap 8800 | awk -F "[:]" '{print $2}')
 # Set running port
 sed -i "s/TEST_PORT = 8800/TEST_PORT = $REST_PORT/g" TestREST.py
+
+cd ..
