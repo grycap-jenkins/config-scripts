60aab2b39f1ce6b09e31b2509098125e13e0ac1c
Jenkins <grycap-jenkins@users.noreply.github.com>
2016-04-26 17:06:12 +0200

    Create execute-long-tests.sh

---
+++ im/execute-long-tests.sh
@@ -0,0 +1,2 @@
+# Execute tests
+nosetests -v test/TestIM.py test/test_im_logic.py test/TestREST.py test/TestREST_JSON.py --with-timer --timer-no-color --with-xunit --with-coverage --cover-erase --cover-html --cover-xml --cover-package=IM
