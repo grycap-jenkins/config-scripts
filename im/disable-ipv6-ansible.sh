b3e1e78dfd48ca0613f9b899d349f37e84967b69
Jenkins <grycap-jenkins@users.noreply.github.com>
2016-04-26 14:56:49 +0200

    Create disable-ipv6-ansible.sh

---
+++ im/disable-ipv6-ansible.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Disable IPv6 in the test machines
+var='    # Disable IPv6\n    - lineinfile: dest=/etc/sysctl.conf regexp="{{ item }}" line="{{ item }} = 1"\n      with_items:\n       - 'net.ipv6.conf.all.disable_ipv6'\n       - 'net.ipv6.conf.default.disable_ipv6'\n       - 'net.ipv6.conf.lo.disable_ipv6'\n      ignore_errors: yes\n    - shell: sysctl -p\n      ignore_errors: yes\n'
+sed  -i "/tasks/a $var" contextualization/conf-ansible.yml
