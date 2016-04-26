2b6dae4599cc4a02dae3a7dbf1401cf4f493ef23
Jenkins <grycap-jenkins@users.noreply.github.com>
2016-04-26 16:08:07 +0200

    Update build-and-launch-docker-container.sh

--- im/build-and-launch-docker-container.sh
+++ im/build-and-launch-docker-container.sh
@@ -8,10 +8,10 @@ cd $1/docker-devel
 # Create image based on 'devel' branch
 docker build -t $2 --no-cache -f Dockerfile . 
 
-cd ..
-
 # Start container
 docker run -d -P --name $2 $3
 
 # Wait for the container
 sleep 10
+
+cd $1
