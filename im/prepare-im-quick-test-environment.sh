a9b5f661ba5a786999e962a00600e8b7ec6d66cf
Jenkins <grycap-jenkins@users.noreply.github.com>
2016-04-29 13:51:19 +0200

    Update prepare-im-quick-test-environment.sh

--- im/prepare-im-quick-test-environment.sh
+++ im/prepare-im-quick-test-environment.sh
@@ -13,4 +13,4 @@ curl -s https://raw.githubusercontent.com/grycap-jenkins/config-scripts/master/i
 curl -s https://raw.githubusercontent.com/grycap-jenkins/config-scripts/master/im/disable-ipv6-ansible.sh | bash -s $1
 
 # Configure tests ports
-curl -s https://raw.githubusercontent.com/grycap-jenkins/config-scripts/master/im/execute-quick-tests.sh | bash -s $1 $2
+curl -s https://raw.githubusercontent.com/grycap-jenkins/config-scripts/master/im/configure-ports-quick-tests.sh | bash -s $1 $2
