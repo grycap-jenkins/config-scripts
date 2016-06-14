2724ad0c8276ca281ee625006f70ecef2d0a90ed
Jenkins <grycap-jenkins@users.noreply.github.com>
2016-06-14 10:23:11 +0200

    Update execute-long-tests.sh

--- im/execute-long-tests.sh
+++ im/execute-long-tests.sh
@@ -1,2 +1,2 @@
 # Execute tests
-nosetests -v test/TestIM.py test/test_im_logic.py test/TestREST.py test/TestREST_JSON.py test/connectors/*.py --with-timer --timer-no-color --with-xunit --with-coverage --cover-erase --cover-html --cover-xml --cover-package=IM
+nosetests -v test/TestIM.py test/test_im_logic.py test/TestREST.py test/TestREST_JSON.py test/connectors/*.py test/SSH.py y test/VMRC.py --with-timer --timer-no-color --with-xunit --with-coverage --cover-erase --cover-html --cover-xml --cover-package=IM
