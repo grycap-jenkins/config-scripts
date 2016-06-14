7f0035a76e610383319180b01eaa0028562d024b
Alfonso Pérez <alpegon3@upvnet.upv.es>
2016-06-14 11:41:30 +0200

    Update execute-quick-tests.sh

--- im/execute-quick-tests.sh
+++ im/execute-quick-tests.sh
@@ -1,2 +1,2 @@
 # Execute tests
-nosetests -v test/QuickTestIM.py test/test_im_logic.py test/TestREST.py test/connectors/*.py test/SSH.py y test/VMRC.py --with-timer --timer-no-color --with-xunit --with-coverage --cover-erase --cover-html --cover-package=IM,connectors
+nosetests -v test/QuickTestIM.py test/test_im_logic.py test/TestREST.py test/connectors/*.py test/SSH.py test/VMRC.py --with-timer --timer-no-color --with-xunit --with-coverage --cover-erase --cover-html --cover-package=IM,connectors
