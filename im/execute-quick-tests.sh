48f3ed4cd5cd4ee954b99b3d68a5fd87c9f1cd80
Miguel Caballer <micafer1@upv.es>
2016-06-17 11:23:24 +0200

    Update tests to new IM tests directories

--- im/execute-quick-tests.sh
+++ im/execute-quick-tests.sh
@@ -1,2 +1,2 @@
 # Execute tests
-nosetests -v test/QuickTestIM.py test/test_im_logic.py test/TestREST.py test/connectors/*.py test/SSH.py test/VMRC.py --with-timer --timer-no-color --with-xunit --with-coverage --cover-erase --cover-html --cover-package=IM
+nosetests -v test/unit/connectors/*.py test/unit/*.py test/functional/*.py test/QuickTestIM.py test/TestREST.py --with-timer --timer-no-color --with-xunit --with-coverage --cover-erase --cover-html --cover-package=IM
