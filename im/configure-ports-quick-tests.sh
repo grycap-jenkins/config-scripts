20299a9466b53bc72397d1315865501c80fe6681
Miguel Caballer <micafer1@upv.es>
2016-06-17 12:14:08 +0200

    Update tests to new IM tests directories

--- im/configure-ports-quick-tests.sh
+++ im/configure-ports-quick-tests.sh
@@ -6,11 +6,11 @@ cd $1/test
 # Get running port
 XMLRPC_PORT=$(docker port $2 8899 | awk -F "[:]" '{print $2}')
 # Set running port
-sed -i "s/TEST_PORT = 8899/TEST_PORT = $XMLRPC_PORT/g" QuickTestIM.py
+sed -i "s/TEST_PORT = 8899/TEST_PORT = $XMLRPC_PORT/g" integration/QuickTestIM.py
 
 # Get running port
 REST_PORT=$(docker port $2 8800 | awk -F "[:]" '{print $2}')
 # Set running port
-sed -i "s/TEST_PORT = 8800/TEST_PORT = $REST_PORT/g" TestREST.py
+sed -i "s/TEST_PORT = 8800/TEST_PORT = $REST_PORT/g" integration/TestREST.py
 
 cd $1
